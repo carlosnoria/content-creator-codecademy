6b89f8659e002b59d8c89156f99b3cea2b801cc6
Carlos Noria <nucinm@gmail.com>
2019-08-14 10:22:09 -0400

    Solucion del proyector

--- js/request-logic.js
+++ js/request-logic.js
@@ -5,4 +5,15 @@
 
 function getContentType(filename) {
   const extension = filename.match(/.*\.([^\.]*)$/)[1];
+  switch(extension) {
+    case 'html':
+      return 'text/html';
+    case 'css':
+      return 'text/css';
+    case 'jpeg':
+    case 'jpg':
+      return 'image/jpeg';
+    case 'unknown':
+      return 'text/plain';
+  }
 }
